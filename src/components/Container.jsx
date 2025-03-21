e3c01e0d920086facac0a706a540ad3d9d2dc01c
AliKhachab <a.khachab620@gmail.com>
2025-03-21 15:40:01 -0400

    refactored into folders

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,15 +1,15 @@
 import Stats from "./Stats";
 import Textarea from "./Textarea";
 import { useState } from "react";
-
+import { FACEBOOK_MAX_CHARS, INSTAGRAM_MAX_CHARS } from "../lib/constants";
 export default function Container() {
   const [text, setText] = useState("");
 
   const stats = {
     numWords: text.split(/\s/).filter((word) => word !== "").length, // split text by spaces, filter out whitespace, count length of array
     numCharacters: text.length,
-    instaCharsLeft: 280-text.length,
-    facebookCharsLeft: 2200-text.length,
+    instaCharsLeft: INSTAGRAM_MAX_CHARS - text.length,
+    facebookCharsLeft: FACEBOOK_MAX_CHARS - text.length,
   };
   return (
     <main className="container">
